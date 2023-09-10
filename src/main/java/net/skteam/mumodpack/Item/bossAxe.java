5e0f0f9e6de0f2a1c55f63e6e071a5fd305fcc06
Darkbonee <Darkbone13@yandex.ru>
2023-09-10 11:12:06 +0300

    update

--- src/main/java/net/skteam/mumodpack/Item/bossAxe.java
+++ src/main/java/net/skteam/mumodpack/Item/bossAxe.java
@@ -6,15 +6,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Rarity;
 
+import java.util.Random;
+
 public class bossAxe extends AxeItem {
 
     public bossAxe(Settings settings){
-        super(ToolMaterials.IRON, 7, -3.15f, settings);
+        super(ToolMaterials.IRON, 9, -3.4f, settings);
     }
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        attacker.setHealth(attacker.getHealth() + (int)(getAttackDamage()/2));
+        Random random = new Random();
+        double multiplier = random.nextDouble(1.7);
+        attacker.setHealth(attacker.getHealth() + (int)(getAttackDamage()/2*multiplier));
         return super.postHit(stack, target, attacker);
     }
 
